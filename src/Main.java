8d1abddcbb94b0ee5d5d51e9eee4b1a0319f09b8
HeinThantAung <100908505+heinthant98@users.noreply.github.com>
2022-08-04 14:24:46 +0630

    ヨーダ記法の削除

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		 List<Anime> animes = List.of(new Anime("ドクターストーン", 5, Genre.ACTION, LocalDate.of(2012, 2, 6)),
-				 	　　　new Anime("進撃の巨人", 4.5F, Genre.ACTION, LocalDate.of(2011, 1, 5)),
-				 	　　　new Anime("ホリミヤ", 5, Genre.ROMANCE, LocalDate.of(2021, 2, 26)),
-				 	　　　new Anime("その 着せ替え人形 ビスク・ドール は恋をする", 5, Genre.ROMANCE, LocalDate.of(2021, 10, 9)),
-				 	　　　new Anime("日常", 3, Genre.COMEDY, LocalDate.of(2020, 12, 6)),
-				 	　　　new Anime("銀魂", 3.5F, Genre.COMEDY, LocalDate.of(2006, 4, 15)),
-				 	　　　new Anime("Haikyuu", 4, Genre.SPORT, LocalDate.of(2014, 4, 9)),
-					　　　new Anime("Haikyuu Season 2", 4.5F, Genre.SPORT, LocalDate.of(2015, 12, 3)));
+				 	      new Anime("進撃の巨人", 4.5F, Genre.ACTION, LocalDate.of(2011, 1, 5)),
+				 	      new Anime("ホリミヤ", 5, Genre.ROMANCE, LocalDate.of(2021, 2, 26)),
+				 	      new Anime("その 着せ替え人形 ビスク・ドール は恋をする", 5, Genre.ROMANCE, LocalDate.of(2021, 10, 9)),
+				 	      new Anime("日常", 3, Genre.COMEDY, LocalDate.of(2020, 12, 6)),
+					      new Anime("銀魂", 3.5F, Genre.COMEDY, LocalDate.of(2006, 4, 15)),
+				 	      new Anime("Haikyuu", 4, Genre.SPORT, LocalDate.of(2014, 4, 9)),
+				 	      new Anime("Haikyuu Season 2", 4.5F, Genre.SPORT, LocalDate.of(2015, 12, 3)));
 
-		Map<Genre, List<Anime>> genreMap = new HashMap<>();
-		genreMap.putAll(animes.stream().collect(Collectors.groupingBy(Anime::getGenre)));
+		Map<Genre, List<Anime>> genreMap = animes.stream().collect(Collectors.groupingBy(Anime::getGenre));
 		
-		System.out.println(" 【すべてのアニメを表示する】");
+		System.out.println("【すべてのアニメを表示する】");
 		animes.forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
 							.formatted(a.getTitle(), a.getGenre(), a.getRating(), toJapaneseFormat(a.getReleasedDate()))));
 
@@ -34,16 +33,14 @@ public class Main {
 		System.out.println("【ジャンルに沿ってアニメを表示する】");
 		genreMap.keySet().stream().forEach(g ->{
 			System.out.println("%s(%d)".formatted(g, sameGenreAnimesCount(animes, g)));
-			genreMap.get(g).stream()
-							.forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
-										  .formatted(a.getTitle(), a.getGenre(), a.getRating(), toJapaneseFormat(a.getReleasedDate()))));
+			genreMap.get(g).stream().forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
+							 .formatted(a.getTitle(), a.getGenre(), a.getRating(), toJapaneseFormat(a.getReleasedDate()))));
 		});
 		
 		System.out.println("【ジャンルに沿ってアニメをジャンルの昇順に並び替えて表示する】");
 		genreMap.keySet().stream().sorted().forEach(g ->{
 			System.out.println("%s(%d)".formatted(g, sameGenreAnimesCount(animes, g)));
-			genreMap.get(g).stream()
-							.forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
+			genreMap.get(g).stream().forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
 										  .formatted(a.getTitle(), a.getGenre(), a.getRating(), toJapaneseFormat(a.getReleasedDate()))));
 		});
 		
@@ -51,10 +48,11 @@ public class Main {
 		animes.stream().filter(a -> a.getRating() == getMaxRating(animes))
 				.forEach(a -> System.out.println("Title: %s, Genre: %s, Rating: %s, Release_Date: %s"
 							  .formatted(a.getTitle(), a.getGenre(), a.getRating(), toJapaneseFormat(a.getReleasedDate()))));
+	
 	}
 
 	private static int sameGenreAnimesCount(List<Anime> animes, Genre genre) {
-		if(0 < animes.size() && null != genre) {
+		if(animes.size() > 0 && Objects.nonNull(genre)) {
 			return animes.stream().filter(a -> a.getGenre().equals(genre)).toList().size();
 		} else {
 			throw new IllegalArgumentException("Animes or Genre must have data.");
@@ -66,10 +64,10 @@ public class Main {
 	}
 
 	private static String toJapaneseFormat(LocalDate releasedDate) {
-		if(null != releasedDate) {
+		if(releasedDate != null) {
 			return releasedDate.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日"));
 		} else {
-			throw new DateTimeException("ReleaseDate cannot be null.");
+			throw new IllegalArgumentException("ReleasedDate cannot be null.");
 		}
 	}
 
